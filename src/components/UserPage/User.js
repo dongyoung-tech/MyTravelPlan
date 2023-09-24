7421f9c7473bb24e45fdc506ad6682dc00c29738
dongyoung-tech <ekzmgjalt55@gmail.com>
2023-09-24 21:07:42 +0900

    2023-09-24

--- src/components/UserPage/User.js
+++ src/components/UserPage/User.js
@@ -47,7 +47,7 @@ const User = () => {
          <h2>{!isLoad && userData.name}</h2>
          <h4>{!isLoad && userData.id}</h4>
          <p>{!isLoad && userData.intro}</p>
-         <button>                      </button>
+         <button>회원정보수정</button>
          <h3 className="sub-topic">찜한 여행지</h3>
          <div className="cart-list">
             {!isLoad && <CartList item={userData.id}/>}
