9fe1f61b808d925a47874102ac93aaeab1363be1
dongyoung-tech <ekzmgjalt55@gmail.com>
2023-10-02 20:56:29 +0900

    2023-10-02

--- src/components/Header/Filter.js
+++ src/components/Header/Filter.js
@@ -3,6 +3,15 @@ import React from "react";
 import { useNavigate } from 'react-router-dom';
 
 const Filter = (props) =>{
+    window.addEventListener('scroll',()=>{
+        if(window.scrollY < 1){
+          document.querySelector('.Nav-Bar').classList.remove('scroll');
+            
+        }
+        else{
+            document.querySelector('.Nav-Bar').classList.add('scroll');
+        }
+    })
     const navigate = useNavigate();
     let topic; 
     if(props.item === "Area") topic = "지역별 검색";
