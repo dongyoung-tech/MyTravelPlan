a9232fca685058794aa14654abfdd6c24495d3d2
dongyoung-tech <ekzmgjalt55@gmail.com>
2023-10-19 10:09:12 +0900

    2023-10-19

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -8,15 +8,17 @@ const Card = (props) =>{
     }
     
     return (
-        <div className="place-box" onClick={clickHandler}>
-        <div className='image-box'>
-          {props.item.firstimage &&  <img src={props.item.firstimage}></img>}
-          {!props.item.firstimage &&  <div className="no-first-image"></div>}
-           </div>
-            <div className='text-box'>
-                <h4>{props.item.title}</h4>
-                <h5>{props.item.addr1}</h5>
-            </div>
+        <div className="place-box">
+            <a href={`/Area/AreaDetail?contentid=${props.item.contentid}`}>
+                <div className='image-box'>
+                {props.item.firstimage &&  <img src={props.item.firstimage}></img>}
+                {!props.item.firstimage &&  <div className="no-first-image"></div>}
+                </div>
+                <div className='text-box'>
+                    <h4>{props.item.title}</h4>
+                    <h5>{props.item.addr1}</h5>
+                </div>
+            </a>
         </div>
     )
 }
